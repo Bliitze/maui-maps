395e1721797043128358d6abfb91d6244f220085
Sebastian Seidel <s.seidel@cayas.de>
2022-05-09 09:58:05 +0200

    Add basic Android MapView handler

---
+++ Platforms/Android/MapHandler.Android.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.Gms.Maps;
+using Android.OS;
+using Java.Interop;
+using Microsoft.Maui.Handlers;
+
+namespace MapControlDemo.Handlers
+{
+    public partial class MapHandler : ViewHandler<MapView, Android.Gms.Maps.MapView>
+    {
+        public MapHandler(IPropertyMapper mapper, CommandMapper commandMapper = null) : base(mapper, commandMapper)
+        { }
+
+        protected override Android.Gms.Maps.MapView CreatePlatformView()
+        {
+            return new Android.Gms.Maps.MapView(Context);
+        }
+
+        protected override void ConnectHandler(Android.Gms.Maps.MapView platformView)
+        {
+            base.ConnectHandler(platformView);
+        }
+    }
+}
+
