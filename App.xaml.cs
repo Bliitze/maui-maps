feca9bd0e652b87e284e4c254ba2338c95a40c9a
Sebastian Seidel <s.seidel@cayas.de>
2022-05-09 09:42:18 +0200

    Initial project set-up with default page changed to MapPage in App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MapControlDemo;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MapPage();
+    }
+}
+
+public class MapPage : ContentPage
+{
+    public MapPage()
+    {
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MapControlDemo;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
